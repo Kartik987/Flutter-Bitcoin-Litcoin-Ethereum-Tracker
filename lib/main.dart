4c135672cac5f3f0afdc19966e5c7409d319823f
rajbeer singh <arorakartik923@gmail.com>
2020-05-15 14:18:57 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.teal[400],
+          primaryColor: Colors.blue,
           scaffoldBackgroundColor: Colors.white),
       home: PriceScreen(),
     );
